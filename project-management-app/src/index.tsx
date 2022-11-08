047b8c98e5c28b90232ab03b9096e9b454902a5c
Alexander Marunich <myrakes4korn@gmail.com>
2022-11-08 12:38:20 +0300

    init: start project management app

---
+++ project-management-app/src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <div>App will be here</div>
+  </React.StrictMode>
+);
