c625ab399ef74dc17919a38d51752d844169f485
nester-dev <nester-dev@gmail.com>
2022-11-10 15:30:07 +0300

    refactor: delete console logs

--- project-management-app/src/components/SignUp/SignUp.tsx
+++ project-management-app/src/components/SignUp/SignUp.tsx
@@ -17,10 +17,6 @@ const SignUp = () => {
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
   };
 
   return (
