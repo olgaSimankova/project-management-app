573677e629fc6406de8de4f3ae30864083f87601
Alexander Marunich <myrakes4korn@gmail.com>
2022-11-11 15:58:37 +0300

    feat: add course logo

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,3 +1,18 @@
+import { Avatar, Box } from '@mui/material';
 import React from 'react';
 
-export const Footer = () => <div>Deployed Footer</div>;
+export const Footer = () => {
+  return (
+    <Box>
+      <Avatar
+        variant={'rounded'}
+        alt="RS Logo"
+        src="https://rs.school/images/rs_school_js.svg"
+        sx={{
+          height: 50,
+          width: 70,
+        }}
+      />
+    </Box>
+  );
+};
