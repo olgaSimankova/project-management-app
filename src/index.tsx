b775363bf4002b333ac21469eac9b86e2982b89d
Alexander Marunich <myrakes4korn@gmail.com>
2022-11-11 12:47:11 +0300

    fix: get rid of basename

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import { App } from './App/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/">
     <App />
   </BrowserRouter>
 );
