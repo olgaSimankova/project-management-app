62e289a38191212a0cd3cdd8e1b39ecb78dd73ab
nester-dev <nester-dev@gmail.com>
2022-11-11 20:02:47 +0300

    feat: add react-hook-form lib

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import { Footer } from 'components/Footer/Footer';
 import { Header } from 'components/Header/Header';
 import { LINKS } from 'constants/constants';
@@ -5,15 +7,17 @@ import { Boards } from 'Pages/Boards';
 import { Error } from 'Pages/Error';
 import { Search } from 'Pages/Search';
 import { Welcome } from 'Pages/Welcome';
-import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { CssBaseline } from '@mui/material';
 import SignIn from '../Pages/SignIn';
 import SignUp from '../Pages/SignUp';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const App = () => (
   <>
     <CssBaseline>
+      <ToastContainer />
       <Header />
       <Routes>
         <Route path={LINKS.welcome} element={<Welcome />} />
