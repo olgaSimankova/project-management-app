20cf9d048bd49a7416195d145acad10ef30276b7
nester-dev <nester.dev@gmail.com>
2022-11-17 17:42:34 +0300

    refactor: delete console.logs

--- src/Pages/Search.tsx
+++ src/Pages/Search.tsx
@@ -1,8 +1,3 @@
 import React from 'react';
-import { useGetUserInfo } from '../hooks/useUserInfo';
 
-export const Search = () => {
-  const user = useGetUserInfo('dima00');
-  console.log(user);
-  return <div>search</div>;
-};
+export const Search = () => <div>Search</div>;
