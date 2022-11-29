b17dfa823fe816ae64c9a959a422edcd08aad59e
Alexander Marunich <myrakes4korn@gmail.com>
2022-11-29 20:44:54 +0300

    feat: add owner and close access to set assignees

--- src/types/types.ts
+++ src/types/types.ts
@@ -179,6 +179,7 @@ export interface AssigneeProps {
   id: string;
   onClose?: ((event: React.SyntheticEvent<Element, Event>, id: string) => void) | undefined;
   register?: UseFormRegister<BoardFormFields>;
+  isDisabled?: boolean;
 }
 
 export interface CheckPasswordModalProps {
