1bacdc31201173c1de17a54bb7013f6676640306
Olga Simankova <o.kotlyarova@gmail.com>
2022-12-05 12:37:12 +0300

    feat: add localization to search page

--- src/Pages/Search.tsx
+++ src/Pages/Search.tsx
@@ -1,21 +1,26 @@
 import { Box, Typography } from '@mui/material';
 import SearchForm from 'features/SearchForm';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
-export const Search = () => (
-  <Box
-    sx={{
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'start',
-      flexDirection: 'column',
-      minHeight: 'calc(100vh - 114px)',
-      padding: '1.5rem 0.5rem',
-    }}
-  >
-    <Typography variant="h5" color="palette.secondary.light">
-      Search tasks
-    </Typography>
-    <SearchForm />
-  </Box>
-);
+export const Search = () => {
+  const { t } = useTranslation();
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'start',
+        flexDirection: 'column',
+        minHeight: 'calc(100vh - 114px)',
+        padding: '1.5rem 0.5rem',
+      }}
+    >
+      <Typography variant="h5" color="palette.secondary.light">
+        {t('searchTasks')}
+      </Typography>
+      <SearchForm />
+    </Box>
+  );
+};
